beb61cf924a0f23435d8b64628f7fcc3024ad0ed
Sibghatullah Veedy <sibghatullahveedy@gmail.com>
2020-07-03 12:55:41 -0700

    gmailPractice Repo

---
+++ src/main/java/gmailPractice/gmailPractice/App.java
@@ -0,0 +1,13 @@
+package gmailPractice.gmailPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
